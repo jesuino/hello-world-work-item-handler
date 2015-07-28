47e648073e6dbcf94417185e6cbd233330f0cdc7
jesuino <william.a.siqueira@gmail.com>
2015-07-28 16:08:32 -0300

    correcting the WIH

--- hello-workitemhandler/src/main/java/org/jugvale/jbpm/workitemhandler/HelloWorkItemHandler.java
+++ hello-workitemhandler/src/main/java/org/jugvale/jbpm/workitemhandler/HelloWorkItemHandler.java
@@ -1,5 +1,7 @@
 package org.jugvale.jbpm.workitemhandler;
 
+import java.util.HashMap;
+
 import org.drools.core.process.instance.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemManager;
@@ -13,6 +15,7 @@ public class HelloWorkItemHandler implements WorkItemHandler {
 
 	public void executeWorkItem(WorkItem wi, WorkItemManager wim) {
 		System.out.println("Hello World!");
+		wim.completeWorkItem(wi.getId(), new HashMap<String, Object>());
 	}
 
 }
